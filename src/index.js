747cfcbe3a06a36eb306a9ca5e61a12f11f75c60
bhavana <bhavana@tradelab.in>
2024-04-13 18:19:11 +0530

    changes related to grid

--- src/index.js
+++ src/index.js
@@ -1,17 +1,188 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ButtonToolbar, MenuItem, DropdownButton, Dropdown } from 'react-bootstrap';
+import { Grid } from './Grid';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+class Buttons extends React.Component {
+
+	handleSelect = (evt) => {
+		this.props.gridSize(evt);
+	}
+
+	render() {
+		return (
+      <div className="center">
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <ButtonToolbar>
+              <button className="btn btn-default" onClick={this.props.playButton}>
+                  Play
+              </button>
+              <button className="btn btn-default" onClick={this.props.pauseButton}>
+                  Pause
+              </button>
+              <button className="btn btn-default" onClick={this.props.clear}>
+                  Clear
+              </button>
+              <button className="btn btn-default" onClick={this.props.slow}>
+                  Slow
+              </button>
+              <button className="btn btn-default" onClick={this.props.fast}>
+                  Fast
+              </button>
+              <button className="btn btn-default" onClick={this.props.seed}>
+                  Seed
+              </button>
+          </ButtonToolbar>
+      </div>
+  </div>
+			)
+	}
+}
+
+class Main extends React.Component {
+	constructor() {
+		super();
+		this.speed = 100;
+		this.rows = 30;
+		this.cols = 50;
+
+		this.state = {
+			generation: 0,
+			gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+		}
+	}
+
+	selectBox = (row, col) => {
+		let gridCopy = arrayClone(this.state.gridFull);
+		gridCopy[row][col] = !gridCopy[row][col];
+		this.setState({
+			gridFull: gridCopy
+		});
+	}
+
+	seed = () => {
+		let gridCopy = arrayClone(this.state.gridFull);
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				if (Math.floor(Math.random() * 4) === 1) {
+					gridCopy[i][j] = true;
+				}
+			}
+		}
+		this.setState({
+			gridFull: gridCopy
+		});
+	}
+
+	playButton = () => {
+		clearInterval(this.intervalId);
+		this.intervalId = setInterval(this.play, this.speed);
+	}
+
+	pauseButton = () => {
+		clearInterval(this.intervalId);
+	}
+
+	slow = () => {
+		this.speed = 1000;
+		this.playButton();
+	}
+
+	fast = () => {
+		this.speed = 100;
+		this.playButton();
+	}
+
+	clear = () => {
+		var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+		this.setState({
+			gridFull: grid,
+			generation: 0
+		});
+	}
+
+	gridSize = (size) => {
+		switch (size) {
+			case "1":
+				this.cols = 20;
+				this.rows = 10;
+			break;
+			case "2":
+				this.cols = 50;
+				this.rows = 30;
+			break;
+			default:
+				this.cols = 70;
+				this.rows = 50;
+		}
+		this.clear();
+
+	}
+
+	play = () => {
+		let g = this.state.gridFull;
+		let g2 = arrayClone(this.state.gridFull);
+
+		for (let i = 0; i < this.rows; i++) {
+		  for (let j = 0; j < this.cols; j++) {
+		    let count = 0;
+		    if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;  
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+		    if (!g[i][j] && count === 3) g2[i][j] = true;
+		  }
+		}
+		this.setState({
+		  gridFull: g2,
+		  generation: this.state.generation + 1
+		});
+
+	}
+
+	componentDidMount() {
+		this.seed();
+		this.playButton();
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>The Game of Life</h1>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Buttons
+                        playButton={this.playButton}
+                        pauseButton={this.pauseButton}
+                        slow={this.slow}
+                        fast={this.fast}
+                        clear={this.clear}
+                        seed={this.seed}
+                        gridSize={this.gridSize}
+                    />
+                    <Grid
+                        gridFull={this.state.gridFull}
+                        rows={this.rows}
+                        cols={this.cols}
+                        selectBox={this.selectBox}
+                    />
+                </div>
+				<h2>Generations: {this.state.generation}</h2>
+			</div>
+		);
+	}
+}
+
+function arrayClone(arr) {
+	return JSON.parse(JSON.stringify(arr));
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
